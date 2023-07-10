110683681265f5ab0bab331d1c3b16c347a9d6cc
Takayama Fumihiko <tekezo@pqrs.org>
2023-07-10 09:22:42 +0900

    exit 1 if error

--- scripts/update-gh-pages.sh
+++ scripts/update-gh-pages.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-curl \
+result=$(curl -s \
   -X POST \
   -H "Content-Type: application/json" \
   -d "{\"secret\":\"$GH_PAGES_UPDATER_SECRET\"}" \
-  https://pqrs.org/ke-gh-pages-updater/update-gh-pages
+  https://pqrs.org/ke-gh-pages-updater/update-gh-pages)
+
+(echo $result | grep error) && exit 1
+exit 0
