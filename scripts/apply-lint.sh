8670e748ec704f560bbdb82a7c2b4278c1b4feaa
Takayama Fumihiko <tekezo@pqrs.org>
2023-08-21 08:32:49 +0900

    Remove checked message from apply-lint.sh

--- scripts/apply-lint.sh
+++ scripts/apply-lint.sh
@@ -8,4 +8,3 @@ karabiner_cli="${topdir}/bin/karabiner_cli"
 lint="'$karabiner_cli' --lint-complex-modifications "
 
 ${topdir}/bin/karabiner_cli --lint-complex-modifications "$@"
-echo "Checked $(echo "$output" | wc -l | sed 's| ||g') files"
