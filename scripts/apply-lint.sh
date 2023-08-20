c4f1ab14d0ce9c267041de820ccc22268b2daf66
Takayama Fumihiko <tekezo@pqrs.org>
2023-08-21 08:30:13 +0900

    Update scripts/apply-lint.sh for set -e

--- scripts/apply-lint.sh
+++ scripts/apply-lint.sh
@@ -7,10 +7,5 @@ topdir="$(dirname $0)/.."
 karabiner_cli="${topdir}/bin/karabiner_cli"
 lint="'$karabiner_cli' --lint-complex-modifications "
 
-output=$(${topdir}/bin/karabiner_cli --lint-complex-modifications "$@")
-if [ $? -ne 0 ]; then
-  echo "$output"
-  exit 1
-else
-  echo "Checked $(echo "$output" | wc -l | sed 's| ||g') files"
-fi
+${topdir}/bin/karabiner_cli --lint-complex-modifications "$@"
+echo "Checked $(echo "$output" | wc -l | sed 's| ||g') files"
