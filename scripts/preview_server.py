3dc6cd2699ed269d2a65d02c81e4f620be2a690e
Takayama Fumihiko <tekezo@pqrs.org>
2026-08-16 15:16:27 +0900

    Improve error handling of update-dist.sh

--- scripts/preview_server.py
+++ scripts/preview_server.py
@@ -4,8 +4,22 @@
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from functools import partial
+import pathlib
 import subprocess
 from urllib.parse import urlparse
+
+CORE_DIRECTORY = pathlib.Path(__file__).resolve().parent.parent
+DIST_DIRECTORY = CORE_DIRECTORY.parent / 'dist'
+UPDATE_DIST_SCRIPT = CORE_DIRECTORY / 'scripts/update-dist.sh'
+
+
+def run_update_dist():
+    '''Update dist and raise an exception on failure.'''
+    print('update-dist.sh')
+    subprocess.run(
+        ['bash', str(UPDATE_DIST_SCRIPT)],
+        cwd=CORE_DIRECTORY,
+        check=True)
 
 
 class RequestHandler(SimpleHTTPRequestHandler):
@@ -22,16 +36,24 @@ class RequestHandler(SimpleHTTPRequestHandler):
     def update_dist(self):
         path = urlparse(self.path).path
         if path == "/" or path == "/index.html":
-            print('update-dist.sh')
-            subprocess.run(['bash', 'scripts/update-dist.sh'])
+            run_update_dist()
 
     def do_GET(self):
-        self.update_dist()
+        try:
+            self.update_dist()
+        except subprocess.CalledProcessError:
+            self.send_error(500, 'Failed to update dist')
+            return
         super().do_GET()
 
 
-print('http://localhost:8000')
+def main():
+    '''Run preview server.'''
+    print('http://localhost:8000')
+    handler = partial(RequestHandler, directory=str(DIST_DIRECTORY))
+    httpd = HTTPServer(('localhost', 8000), handler)
+    httpd.serve_forever()
 
-handler = partial(RequestHandler, directory="../dist")
-httpd = HTTPServer(('localhost', 8000), handler)
-httpd.serve_forever()
+
+if __name__ == '__main__':
+    main()
